bfec4ad93f2efe83d78edeb8422c4c2bf7206efb
slowdive <schlickjoe@gmail.com>
2020-08-31 23:05:04 -0700

    post tutor session - moved generator functions to generateMarkdown.js, added github url, license badge, license statement functions. index - completed writeToFile and init functions.

---
+++ README.md
@@ -0,0 +1,47 @@
+# 
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com//)
+
+## Description:
+
+
+
+## Table of Contents:
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation:
+
+To install necessary dependancies, run the following command:
+
+```
+
+```
+
+## Usage:
+
+
+
+## License:
+      
+      This project is licensed under MIT.
+
+## Contributing:
+
+
+
+## Tests:
+
+To run tests run the following command:
+
+```
+
+```
+
+## Questions:
+
+If you have any questions about the repo, open an issue or contact [](undefined) directly at .
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,73 @@
 // function to generate markdown for README
+function projectURL(github, title) {
+  const caseTitle = title.toLowerCase().split(" ").join("-")
+  return `https://github.com/${github}/${caseTitle}`
+}
+
+function licenseBadge(license, github, title) {
+  if (license !== "none") {
+    return `[![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)](${projectURL(github, title)})`
+  }
+  return ""
+}
+
+function licenseSection(license) {
+  if (license !== "none") {
+    return (
+      `## License:
+      
+      This project is licensed under ${license}.`
+    )
+  }
+  return ""
+}
+
 function generateMarkdown(data) {
   return `# ${data.title}
+${licenseBadge(data.license, data.github, data.title)}
 
+## Description:
+
+${data.description}
+
+## Table of Contents:
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation:
+
+To install necessary dependancies, run the following command:
+
+\`\`\`
+${data.installation}
+\`\`\`
+
+## Usage:
+
+${data.usage}
+
+${licenseSection(data.license)}
+
+## Contributing:
+
+${data.contributing}
+
+## Tests:
+
+To run tests run the following command:
+
+\`\`\`
+${data.tests}
+\`\`\`
+
+## Questions:
+
+If you have any questions about the repo, open an issue or contact [${data.github}](${data.URL}) directly at ${data.email}.
 `;
 }
 
